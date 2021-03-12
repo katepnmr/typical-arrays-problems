63b3bed9cb76bd70218bc4a10632b85e4fcd5101
katepnmr <eponomarenko96@mail.ru>
2021-03-12 10:33:04 +0300

    init: write a function index.js in the Typical Arrays Problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(typeof array === "undefined" || array.length === 0) {
+      return 0; 
+    } else {
+      return Math.min.apply(Math, array);
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(typeof array === "undefined" || array.length === 0) {
+      return 0; 
+    } else {
+      return Math.max.apply(Math, array);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(typeof array === "undefined" || array.length === 0) {
+      return 0; 
+    }
+    let sumOfnumbers = array.reduce((accumulator, currentValue) => (accumulator + currentValue));
+    let average = sumOfnumbers / array.length;
+    return average;
 }
+
